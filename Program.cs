70bf598515fe87d2ac4ddf98e1bb8631c185fd5c
Vita Gold <golvi1124@gmail.com>
2025-03-31 14:32:27 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string? userName = "";
+Console.WriteLine("Hello! Welcome to the math game! What is yout name?");
+userName = Console.ReadLine();
+Console.WriteLine($"Okey, {userName}, that's great that you're working on improving yourself! Let's start!\n");
+Console.WriteLine("Press any key to continue.");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine(@"What game would you like to play?
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+V - View Previous Games");
+
