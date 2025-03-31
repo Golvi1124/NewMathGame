83a78bba52fcf444ff6e04d34b1ae7363669d79a
Vita Gold <golvi1124@gmail.com>
2025-03-31 15:19:05 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿string? userName = "";
-Console.WriteLine("Hello! Welcome to the math game! What is yout name?");
-userName = Console.ReadLine();
-Console.WriteLine($"Okey, {userName}, that's great that you're working on improving yourself! Let's start!\n");
+﻿Console.WriteLine($"What's your name?");
+string name = Console.ReadLine();
+DateTime date = DateTime.Now;
+int gamesPlayed = 0;
+char userOption;
+decimal averageScore;
+bool isScoreEmpty = true;
+
+Console.WriteLine($"Hello {name}! Today is {date}. This is your math's game. It's great that you're working on improving yourself.\n");
 Console.WriteLine("Press any key to continue.");
-Console.ReadLine();
+Console.ReadKey();
 Console.Clear();
+Console.WriteLine($"Games played: {gamesPlayed}");
 Console.WriteLine(@"What game would you like to play?
 A - Addition
 S - Subtraction
@@ -12,3 +18,5 @@ M - Multiplication
 D - Division
 V - View Previous Games");
 
+userOption = Console.ReadKey().KeyChar;
+Console.WriteLine($"\nYou chose the option: {userOption}");
