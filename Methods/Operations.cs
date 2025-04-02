fc5bd15fc500767807b6bb1d0ceb8d7d1e4c8423
Vita Gold <golvi1124@gmail.com>
2025-04-02 13:50:30 +0200

    02.04.

---
+++ Methods/Operations.cs
@@ -0,0 +1,48 @@
+﻿namespace NewMathGame.Methods;
+
+public static class Operations
+{
+    public static void AdditionGame(string message)
+    {
+        Console.Clear();
+
+        var random = new Random();
+        var score = 0;
+
+        int firstNumber = random.Next(1, 9);
+        int secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect!");
+        }
+    }
+
+    public static void SubtractionGame(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void MultiplicationGame(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void DivisionGame(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void ViewPreviousGames(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,59 @@
-﻿Console.WriteLine($"What's your name?");
-string name = Console.ReadLine();
-DateTime date = DateTime.Now;
-int gamesPlayed = 0;
-char userOption;
-decimal averageScore;
-bool isScoreEmpty = true;
+﻿using NewMathGame.Methods;
 
-Console.WriteLine($"Hello {name}! Today is {date}. This is your math's game. It's great that you're working on improving yourself.\n");
-Console.WriteLine("Press any key to continue.");
-Console.ReadKey();
-Console.Clear();
-Console.WriteLine($"Games played: {gamesPlayed}");
-Console.WriteLine(@"What game would you like to play?
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
-V - View Previous Games");
+class Program
+{
+    static void Main(string[] args)
+    {
 
-userOption = Console.ReadKey().KeyChar;
-Console.WriteLine($"\nYou chose the option: {userOption}");
+        Console.WriteLine($"What's your name?");
+        string name = Console.ReadLine();
+        DateTime date = DateTime.Now;
+        int gamesPlayed = 0;
+        char userOption;
+        decimal averageScore;
+        bool isScoreEmpty = true;
+
+        Console.WriteLine($"Hello {name}! Today is {date}. This is your math's game. It's great that you're working on improving yourself.\n");
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+        Console.Clear();
+        Console.WriteLine($"Games played: {gamesPlayed}");
+        Console.WriteLine(@"What game would you like to play?
+        A - Addition
+        S - Subtraction
+        M - Multiplication
+        D - Division
+        V - View Previous Games
+        Q - Quit The Program");
+
+        userOption = Console.ReadKey().KeyChar;
+        Console.WriteLine($"\nYou chose the option: {userOption}");
+
+        switch (char.ToLower(userOption))
+        {
+            case 'a':
+                Operations.AdditionGame("You're playing an addition game");
+                break;
+            case 's':
+                Operations.SubtractionGame("You're playing a subtraction game");
+                break;
+            case 'm':
+                Operations.MultiplicationGame("You're playing a multiplication game");
+                break;
+            case 'd':
+                Operations.DivisionGame("You're playing a division game");
+                break;
+            case 'v':
+                Operations.ViewPreviousGames("List of Games");
+                break;
+            case 'q':
+                Console.WriteLine("Goodbye");
+                break;
+            default:
+                Console.WriteLine("Invalid input");
+                break;
+        }
+
+
+    }
+}
