c6cc85f2b0d336723e381806f9b3132fb36c97f7
Vita Gold <golvi1124@gmail.com>
2025-04-02 16:00:21 +0200

    done with operatins

--- Methods/Operations.cs
+++ Methods/Operations.cs
@@ -1,4 +1,6 @@
-﻿namespace NewMathGame.Methods;
+﻿// need too add after wrong input user should be able to try again
+
+namespace NewMathGame.Methods;
 
 public static class Operations
 {
@@ -10,15 +12,15 @@ public static class Operations
         int firstNumber;
         int secondNumber;
 
-        
         Console.WriteLine("How many times would you like to play?");
         var input = Console.ReadLine();
+
         if (int.TryParse(input, out var numberOfRounds))
         {
             for (int i = 0; i < numberOfRounds; i++)
             {
-                firstNumber = random.Next(1, 9);
-                secondNumber = random.Next(1, 9);
+                firstNumber = random.Next(1, 51);
+                secondNumber = random.Next(1, 51);
 
                 Console.WriteLine($"{firstNumber} + {secondNumber}");
                 var result = Console.ReadLine();
@@ -41,25 +43,21 @@ public static class Operations
         }
     }
 
-    public static void SubtractionGame()
+    public static void SubtractionGame() // first number always greater than second number
     {
-        var random = new Random();
-        var score = 0;
-
-        int firstNumber;
-        int secondNumber;
-
-
         Console.WriteLine("How many times would you like to play?");
         var input = Console.ReadLine();
+
         if (int.TryParse(input, out var numberOfRounds))
         {
+            var random = new Random();
+            var score = 0;
+
             for (int i = 0; i < numberOfRounds; i++)
             {
-                firstNumber = random.Next(1, 21);
-                secondNumber = random.Next(1, 21);
-
-                //make it so that the first number is always greater than the second number
+                var subtractionNumbers = GetSubtractionNumbers(random);
+                var firstNumber = subtractionNumbers[0];
+                var secondNumber = subtractionNumbers[1];
 
                 Console.WriteLine($"{firstNumber} - {secondNumber}");
                 var result = Console.ReadLine();
@@ -97,13 +95,13 @@ public static class Operations
         {
             for (int i = 0; i < numberOfRounds; i++)
             {
-                firstNumber = random.Next(1, 9);
-                secondNumber = random.Next(1, 9);
+                firstNumber = random.Next(1, 11);
+                secondNumber = random.Next(1, 11);
 
-                Console.WriteLine($"{firstNumber} + {secondNumber}");
+                Console.WriteLine($"{firstNumber} * {secondNumber}");
                 var result = Console.ReadLine();
 
-                if (int.TryParse(result, out var userAnswer) && userAnswer == firstNumber + secondNumber)
+                if (int.TryParse(result, out var userAnswer) && userAnswer == firstNumber * secondNumber)
                 {
                     Console.WriteLine("Correct!");
                     score++;
@@ -123,11 +121,69 @@ public static class Operations
 
     public static void DivisionGame()
     {
-        
+        Console.WriteLine("How many times would you like to play?");
+        var input = Console.ReadLine();
+        if (int.TryParse(input, out var numberOfRounds))
+        {
+            var random = new Random();
+            var score = 0;
+
+            for (int i = 0; i < numberOfRounds; i++)
+            {
+                var divisionNumbers = GetDivisionNumbers(random);
+                var firstNumber = divisionNumbers[0];
+                var secondNumber = divisionNumbers[1];
+
+                Console.WriteLine($"{firstNumber} / {secondNumber}");
+                var result = Console.ReadLine();
+
+                if (int.TryParse(result, out var userAnswer) && userAnswer == firstNumber / secondNumber)
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
+            }
+            Console.WriteLine($"Game over. Your final score is {score} out of {numberOfRounds}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
     }
 
     public static void ViewPreviousGames()
     {
-        
+
+    }
+
+    private static int[] GetDivisionNumbers(Random random)
+    {
+        var firstNumber = random.Next(1, 99);
+        var secondNumber = random.Next(1, 99);
+
+        while (firstNumber % secondNumber != 0)
+        {
+            firstNumber = random.Next(1, 99);
+            secondNumber = random.Next(1, 99);
+        }
+
+        return new int[] { firstNumber, secondNumber };
+    }
+
+    private static int[] GetSubtractionNumbers(Random random)
+    {
+        var firstNumber = random.Next(1, 51);
+        var secondNumber = random.Next(1, 51);
+        while (firstNumber < secondNumber)
+        {
+            firstNumber = random.Next(1, 51);
+            secondNumber = random.Next(1, 51);
+        }
+        return new int[] { firstNumber, secondNumber };
     }
 }
+
